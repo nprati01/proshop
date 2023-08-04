5b5882f794540fca7ce3ee6f8303c20241e7a95f
nprati01 <nicole.e.prati@gmail.com>
2023-08-04 13:33:35 -0400

    product page setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,13 @@ import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
 import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomePage />} />
+      <Route path='/product/:id' element={<ProductPage />} />
     </Route>
 
   )
