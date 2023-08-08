bddda75dde06c502fe19abf9f5a3a2e87dbb3496
nprati01 <nicole.e.prati@gmail.com>
2023-08-08 18:49:47 -0400

    initialize redux

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,29 +1,34 @@
 import React from "react";
-import { useEffect, useState } from "react";
+
 import { Row, Col } from "react-bootstrap";
-import Product from '../components/Product'
-import axios from 'axios'
+import Product from "../components/Product";
+import Loader from "../components/Loader";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 
-
+//state is held at app level -updated through actions
+// dispatched to a reducer function and the updated state is sent down to other components that need it
+//so all parts of the app have the changes to state
 const HomePage = () => {
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products');
-            setProducts(data);
-        }
-        fetchProducts();
-    }, [])
+  const { data: products, isLoading, error } = useGetProductsQuery();
+
   return (
     <>
-      <h1>Newest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <div> {error?.data?.message || error.error}</div>
+      ) : (
+        <>
+          <h1>Newest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
