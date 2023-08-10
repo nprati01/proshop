dc1104fa27bff9231d2dc21c4385fe945ea02c23
nprati01 <nicole.e.prati@gmail.com>
2023-08-10 13:10:31 -0400

    setup user controller and routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,73 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../models/userModel.js";
+
+//Auth user & get token
+//route POST/api/users/login
+//Public
+const authUser = asyncHandler(async (req, res) => {
+  res.send("auth user");
+});
+//register user
+//route POST/api/users
+//Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.send("register user");
+});
+//Logout user / clear cookie
+//route POST/api/users/logout
+//Private
+const logoutUser = asyncHandler(async (req, res) => {
+  res.send("logout user");
+});
+//Get user Profile
+//route GEt/api/users/profile
+//Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send("user profile");
+});
+//Update user Profile
+//route PUT/api/users/profile
+//Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.send("update user profile");
+});
+
+// Get users
+//route GET/api/users
+//Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  res.send("get users");
+});
+
+// Delete users
+//route DELETE/api/users/:id
+//Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send("delete User");
+});
+
+// Get User By ID
+//route Get/api/users/:id
+//Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  res.send("Get user By ID");
+});
+
+// Update User
+//route PUT/api/users/:id
+//Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  res.send("Update User");
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import { notFound, errorHandler } from './middleware/errorHandler.js';
+
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 const port = process.env.PORT || 8000
 
 connectDB();
@@ -11,12 +14,10 @@ const app = express();
 app.get('/', (req,res) => {
     res.send('API is running...');
 })
-app.get('/api/products', (req, res) => {
-    res.json(products);
-})
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use(notFound);
+app.use(errorHandler);
+
 
 app.listen(port, () => console.log(`server running on port ${port}`));
