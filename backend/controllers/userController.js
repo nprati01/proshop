47d4f6afecd1721e0383a86e13183c21628c7b93
nprati01 <nicole.e.prati@gmail.com>
2023-08-10 13:31:57 -0400

    implement jwt token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,13 +5,56 @@ import User from "../models/userModel.js";
 //route POST/api/users/login
 //Public
 const authUser = asyncHandler(async (req, res) => {
-  res.send("auth user");
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if ( user && (await user.matchPassword(password))) {
+        generateToken(res, user._id);
+
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
+
 });
 //register user
 //route POST/api/users
 //Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.send("register user");
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+    });
+
+    if (user) {
+        generateToken(res, user._id);
+
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data')
+    }
 });
 //Logout user / clear cookie
 //route POST/api/users/logout
---
+++ backend/utils/generateToken.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken'
+
+const generateToken = (res, userId) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+
+    // Set as HTTP-only cookie
+    res.cookie('jwt', token,{
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development', // user secure cookies in production
+        sameSite: 'strict', //Prevent CSRF attacks
+        maxAge: 30 * 24 * 60 *1000, //30 days 
+    })
+}
